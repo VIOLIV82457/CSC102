2b52478da2bf4a2e09c09fdfd8676dddf811a08b
Victoria Olivarez <volivarez82457@uat.edu>
2025-10-16 13:39:38 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -28,3 +28,41 @@ function rollDice(){
 let die = Math.random() * 6;
 return Math.ceil(die);
 }
+
+
+
+
+            function validateForm(){
+                let FirstName = document.getElementById("txtFirstName").value.trim();
+
+                let LastName = document.getElementById("txtLastName").value.trim();
+
+                let ZipCode = document.getElementById("txtZipCode").value.trim();
+
+
+                console.log("FirstName=" + FirstName);
+                console.log("LastName=" + LastName);
+                console.log("ZipCode=" + ZipCode);
+
+                FullName = FirstName + " " + LastName;
+
+                console.log("FullName=" + FullName);
+
+                if (FullName.length > 20 || FullName.length ==1){
+                    message = "Invalid name, please try again.";
+
+                    console.log("Invalid name")
+                }
+
+                else if (ZipCode.length != 5){
+                     message = "Invalid Zip Code, please try again.";
+
+                    console.log("Invalid Zip Code")
+                }
+
+                else{
+                    message = "Welcome," +FirstName + ". The secret word is Coders.";
+                }
+
+                document.getElementById("divMessage").textContent = message
+            }
